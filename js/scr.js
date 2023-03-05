f5bfb76b8c7472f6801bfba28799535f5a60de66
mila131183 <121664724+mila131183@users.noreply.github.com>
2023-03-05 20:50:07 +0400

    Add files via upload

--- js/scr.js
+++ js/scr.js
@@ -1,13 +1,10 @@
-let butl = document.querySelector('butl');
-let ogPopUpl = document.querySelector('og-popupl');
-let closePopUp = document.querySelector('close-popup');
-closePopUp = evt => {
-	target = evt.target;
-	if (target === ogPopUpl.closest('og-popup', close)) {
-		ogPopUpl.opacity = 0;
-	}
-}
-setTimeout() {
-	ogPopUpl.visibility = 'hidden';
-}
+let butl = document.querySelector('.butl');
+let ogPopUpl = document.querySelector('.og-popupl');
+let closePopUp = document.querySelector('.close-popup');
+
+butl.addEventListener('click', showPopUp);
+function showPopUp(evt) {
+	evt.preventDefault();
+	ogPopUpl.classList.toggle('show-popup');
+};
 
